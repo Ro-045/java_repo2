8aeccfa7f0470a3aadbfbf9e2ebc569caeedae7a
Ro-045 <pratapnaidu016@gmail.com>
2024-06-19 12:47:49 +0530

    programmes

---
+++ project_programs/src/Sam1.java
@@ -0,0 +1,6 @@
+public class Sam1 {
+    public static void main(String[] args) {
+        System.out.println("hello pratap!");
+        System.out.println("hyjkg");
+    }
+}
---
+++ project_programs/src/Sam2.java
@@ -0,0 +1,5 @@
+public class Sam2 {
+    public static void main(String[] args) {
+        System.out.println("fjfgj");
+    }
+}
